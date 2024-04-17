b11f2c4b60f622301d7bc7634eddc7ef53f096b3
Neielmonti <neielmonti@gmail.com>
2024-04-17 14:08:24 -0300

    Agregado de configuracion

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,11 +4,21 @@ require 'helpers.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Dotenv\Dotenv;
+
+use Paw\Core\Config;
 
 require "routes.php";
 
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$config = new Config;
+
 $log = new Logger('mvc-app');
-$log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
+$handler = new StreamHandler($config->get("LOG_PATH"));
+$handler->setLevel($config->get("LOG_LEVEL"));
+$log->pushHandler($handler);
 
 //$whoops = new \Whoops\Run;
 //$whoops->pushHandler(new \Whoops\Handler\PrettyHandler);
