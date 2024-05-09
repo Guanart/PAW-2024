d52e0afa84ecb14a796333601a8475b63e6d51dc
Gonzalo Benito <60377535+Guanart@users.noreply.github.com>
2024-05-09 16:39:10 -0300

    Refactor ReservationPlan.js

--- public/js/components/reservationPlan.js
+++ public/js/components/reservationPlan.js
@@ -1,4 +1,7 @@
-class reservationPlan {
+class ReservationPlan {
+    // URL del archivo SVG
+    urlSVG = '/../../images/svg/PlanoSucursalA.svg'; // Reemplaza con la URL de tu archivo SVG
+    
     constructor(pContenedor) {
         // Conseguir nodo NAV
         let contenedor = pContenedor.tagName
@@ -13,83 +16,81 @@ class reservationPlan {
         let css = tools.nuevoElemento('link', '', {
             rel: 'stylesheet',
             href: '/js/components/styles/reservationPlan.css'
-            });
+        });
         document.head.appendChild(css);
 
-        // URL del archivo SVG
-        const urlSVG = '/../../images/svg/PlanoSucursalA.svg'; // Reemplaza con la URL de tu archivo SVG
+        // Llamar a la función para cargar el contenido del archivo SVG
+        this.cargarSVG(contenedor);
+    }
 
-        // Función para cargar el contenido del archivo SVG desde una URL
-        async function cargarSVG() {
-            try {
-                // Realiza la solicitud para cargar el archivo SVG
-                const response = await fetch(urlSVG);
+    // Función para cargar el contenido del archivo SVG desde una URL
+    async cargarSVG(contenedor) {
+        try {
+            // Realiza la solicitud para cargar el archivo SVG
+            const response = await fetch(this.urlSVG);
 
-                // Verifica si la solicitud fue exitosa
-                if (!response.ok) {
-                    throw new Error('No se pudo cargar el archivo SVG');
+            // Verifica si la solicitud fue exitosa
+            if (!response.ok) {
+                throw new Error('No se pudo cargar el archivo SVG');
+            }
+
+            // Convierte la respuesta en texto
+            const svgText = await response.text();
+
+            // Usar DOMParser para analizar la cadena SVG y crear un nodo DOM
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(svgText, 'image/svg+xml');
+
+            // Obtener el elemento SVG
+            const svgNode = doc.documentElement;
+
+            contenedor.appendChild(svgNode);
+
+            const elementosMesa = document.querySelectorAll('.mesa');
+            console.log(elementosMesa);
+            this.agregarListeners(elementosMesa)
+
+        } catch (error) {
+            console.error('Error al cargar el archivo SVG:', error);
+        }
+    }
+
+    agregarListeners(elementosMesa) {
+        const inputTexto = document.getElementById('texto');
+        const mesasSeleccionadas = new Set();
+        // Itera sobre cada elemento de la clase 'mesa' y agrega un eventListener de 'click'
+        elementosMesa.forEach(elemento => {
+            elemento.addEventListener('click', function (event) {
+                // Usamos evento.target para obtener el elemento que se ha hecho clic
+                const elementoClicado = event.target;
+
+                if (elementoClicado.classList.contains("mesaSeleccionada")) {
+                    event.target.classList.remove("mesaSeleccionada");
+                } else {
+                    event.target.classList.add("mesaSeleccionada");
                 }
 
-                // Convierte la respuesta en texto
-                const svgText = await response.text();
-                
-                // Usar DOMParser para analizar la cadena SVG y crear un nodo DOM
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(svgText, 'image/svg+xml');
-                                
-                // Obtener el elemento SVG
-                const svgNode = doc.documentElement;
+                // Usamos closest() para subir en el árbol DOM hasta el <g> más cercano
+                const grupoG = elementoClicado.closest('g');
 
-                contenedor.appendChild(svgNode);
+                // Verifica que el grupo <g> tiene un ID y extrae el ID
+                if (grupoG && grupoG.id) {
+                    const idMesa = grupoG.id;
 
-                const elementosMesa = document.querySelectorAll('.mesa');
-                console.log(elementosMesa);
-                agregarListeners(elementosMesa)
-
-            } catch (error) {
-                console.error('Error al cargar el archivo SVG:', error);
-            }
-        }
-
-        // Llamar a la función para cargar el contenido del archivo SVG
-        cargarSVG();
-
-        function agregarListeners(elementosMesa) {
-            const inputTexto = document.getElementById('texto');
-            const mesasSeleccionadas = new Set();
-            // Itera sobre cada elemento de la clase 'mesa' y agrega un eventListener de 'click'
-            elementosMesa.forEach(elemento => {
-                elemento.addEventListener('click', function(event) {
-                    // Usamos evento.target para obtener el elemento que se ha hecho clic
-                    const elementoClicado = event.target;
-
-                    if (elementoClicado.classList.contains("mesaSeleccionada")) {
-                        event.target.classList.remove("mesaSeleccionada");
+                    // Si el ID ya está en el conjunto, lo elimina; de lo contrario, lo agrega
+                    if (mesasSeleccionadas.has(idMesa)) {
+                        mesasSeleccionadas.delete(idMesa);
                     } else {
-                        event.target.classList.add("mesaSeleccionada");
+                        mesasSeleccionadas.add(idMesa);
                     }
-            
-                    // Usamos closest() para subir en el árbol DOM hasta el <g> más cercano
-                    const grupoG = elementoClicado.closest('g');
-            
-                    // Verifica que el grupo <g> tiene un ID y extrae el ID
-                    if (grupoG && grupoG.id) {
-                        const idMesa = grupoG.id;
-            
-                        // Si el ID ya está en el conjunto, lo elimina; de lo contrario, lo agrega
-                        if (mesasSeleccionadas.has(idMesa)) {
-                            mesasSeleccionadas.delete(idMesa);
-                        } else {
-                            mesasSeleccionadas.add(idMesa);
-                        }
-            
-                        // Actualiza el campo de texto con los IDs de las mesas seleccionadas
-                        inputTexto.value = Array.from(mesasSeleccionadas).join(', ');
-                    } else {
-                        console.log('El elemento clicado no está dentro de un <g> con ID.');
-                    }
-                });
+
+                    // Actualiza el campo de texto con los IDs de las mesas seleccionadas
+                    inputTexto.value = Array.from(mesasSeleccionadas).join(', ');
+                } else {
+                    console.log('El elemento clicado no está dentro de un <g> con ID.');
+                }
             });
-        } 
+        });
     }
+
 }
